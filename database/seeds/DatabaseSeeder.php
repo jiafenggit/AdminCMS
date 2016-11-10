170d176c0ed431659d385730aea5a91ab775ac40
qijingyu <qijingyu2009@gmail.com>
2016-11-10 10:38:19 +0800

    初始化提交

---
+++ database/seeds/DatabaseSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Model::unguard();
+
+//        $this->call(UsersTableSeeder::class);
+//        $this->call(RolesTableSeeder::class);
+//        $this->call(RoleUserTableSeeder::class);
+//        $this->call(PermissionsTableSeeder::class);
+//        $this->call(PermissionRoleTableSeeder::class);
+//        $this->call(NewsTableSeeder::class);
+        $this->call(InformsTableSeeder::class);
+
+        Model::reguard();
+    }
+}
